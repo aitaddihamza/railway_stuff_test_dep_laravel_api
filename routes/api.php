a8395e0ca6c89d43fe360763cf02379070088321
aitaddihamza <aitaddi.hamza365@gmail.com>
2025-06-23 10:02:44 +0100

    updated api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/test_laravel_api', function () {
+    $user = new User([
+        "name" => "test",
+        "eamil", "test@gmail.com",
+        "password" => bcrypt("password"),
+    ]);
+    $user->save();
     return "api is working well ";
 });
